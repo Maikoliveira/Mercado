3e2f37fa42433ef865591c63f5bc809ea440c773
Maikoliveira <maik37oliveira@gmail.com>
2017-06-20 21:21:11 -0300

    Mercado
    Progama de venda de produtos

---
+++ Produto.py
@@ -0,0 +1,21 @@
+class produto:
+    def __init__(self, nome, preco, tipo, estoque):
+        self.nome = nome
+        self.preco = preco
+        self.tipo = tipo
+        self.estoque = estoque
+
+    def getNome(self):
+        return self.nome
+
+    def getPreco(self):
+        return self.preco
+
+    def getTipo(self):
+        return self.tipo
+
+    def getEstoque(self):
+        return self.estoque
+
+    def setEstoque(self, estoque2):
+        self.estoque = estoque2
